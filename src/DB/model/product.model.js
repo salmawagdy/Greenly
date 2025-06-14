1c52571be40650df171c99dcaf10280209a0470a
shadwaadolf <shadwaadolf5@gmail.com>
2025-06-14 05:58:08 +0300

    .

--- src/DB/model/product.model.js
+++ src/DB/model/product.model.js
@@ -1,31 +1,25 @@
-
 import mongoose from "mongoose";
 import { model, Schema } from "mongoose";
 const productSchema = new Schema(
   {
-    name: { type: String, required: true, trim: true 
-    },
+    name: { type: String, required: true, trim: true },
 
-    shortdescription: { type: String, min: 0, max: 200 
-    },
+    shortdescription: { type: String, min: 0, max: 200 },
 
-    longdescription: { type: String, min: 0, max: 1000 
-    },
+    longdescription: { type: String, min: 0, max: 1000 },
 
-    price: { type: Number, required: true, min: 0 
-    },
+    price: { type: Number, required: true, min: 0 },
 
-category: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: "Category",
-  required: true
-},
-subCategory: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: "subCategory", 
-  required: true
-}
-,
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    subCategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "subCategory",
+      required: true,
+    },
     stock: { type: Number, default: 0, min: 0 },
 
     imageCover: { type: String },
@@ -33,8 +27,8 @@ subCategory: {
     images: [{ type: String }],
 
     ratingAvg: { type: Number, default: 0 },
-    
-    vendor:{type:String, required:true},
+
+    vendor: { type: String },
   },
   { timestamps: true }
 );
